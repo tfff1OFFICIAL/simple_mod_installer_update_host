744aea5b5e21596d05685fb22d22523722a1140a
tfff1OFFICIAL <tfff1OFFICIAL@users.noreply.github.com>
2018-04-13 15:00:37 +1200

    moved to falcon framework for speed

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
+import falcon
 
 
-@app.route("/currentversion.json")
-def send_version_data():
-    return jsonify({
-        "1.0": "https://s3-ap-southeast-2.amazonaws.com/tfff1-data/simple+mod+installer/simple.mod.installer.v3.0.4.msi"
-    })
+class VersionDataResource(object):
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = ('{"1.0": "https://s3-ap-southeast-2.amazonaws.com/tfff1-data/simple+mod+installer/simple.mod.installer.v3.0.4.msi"}')
+
+
+app = falcon.API()
+
+versions = VersionDataResource()
+
+app.add_route("/versions.json", versions)
